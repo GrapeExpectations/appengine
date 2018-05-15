e58420444d45bda0a334e9e258f31d2dbb9878ab
Todd Radigan <todd.radigan@d2l.com>
2018-05-15 13:13:14 -0400

    update to ServiceClient... now needs module (dosen't do this for you)

--- service/client.go
+++ service/client.go
@@ -1,62 +1,59 @@
 package service
 
 import (
-  "github.com/GrapeExpectations/appengine/errors"
-  "context"
-  "fmt"
-  "google.golang.org/appengine"
-  "google.golang.org/appengine/urlfetch"
-  "net/http"
+	"context"
+	"net/http"
+
+	"google.golang.org/appengine"
+	"google.golang.org/appengine/urlfetch"
 )
 
 type ServiceClient struct {
-  client *http.Client
-  module string
+	client *http.Client
+	module string
 }
 
-func NewServiceClient(ctx context.Context, service string) (*ServiceClient, error) {
-  client := urlfetch.Client(ctx)
+func NewServiceClient(ctx context.Context, module string) *ServiceClient {
+	client := urlfetch.Client(ctx)
 
-  m, err := appengine.ModuleHostname(ctx, service, "", "")
-	if err != nil {
-    msg := fmt.Sprintf("error finding service [%v]: %v", service, err)
-    return nil, errors.New(http.StatusInternalServerError, msg)
-	}
-
-  return &ServiceClient {client, m}, nil
+	return &ServiceClient{client, module}
 }
 
 func (c *ServiceClient) Delete(r *Request) (*http.Response, error) {
-  return c.Request("DELETE", r)
+	r.Method("DELETE")
+	return c.Do(r)
+}
+
+func (c *ServiceClient) Do(r *Request) (*http.Response, error) {
+	r.Protocol(protocol())
+	r.Module(c.module)
+
+	req, err := r.HTTPRequest()
+	if err != nil {
+		return nil, err
+	}
+
+	return c.client.Do(req)
 }
 
 func (c *ServiceClient) Get(r *Request) (*http.Response, error) {
-  return c.Request("GET", r)
+	r.Method("GET")
+	return c.Do(r)
 }
 
 func (c *ServiceClient) Post(r *Request) (*http.Response, error) {
-  return c.Request("POST", r)
+	r.Method("POST")
+	return c.Do(r)
 }
 
 func (c *ServiceClient) Put(r *Request) (*http.Response, error) {
-  return c.Request("POST", r)
-}
-
-func (c *ServiceClient) Request(method string, r *Request) (*http.Response, error) {
-  url := fmt.Sprintf("%s%s%s", protocol(), c.module, r.path)
-  req, err := http.NewRequest(method, url, r.body)
-	if err != nil {
-    return nil, err
-	}
-
-  req.Header = r.Header
-
-  return c.client.Do(req)
+	r.Method("PUT")
+	return c.Do(r)
 }
 
 func protocol() string {
-  if appengine.IsDevAppServer() {
-    return "http://"
-  }
-  return "https://"
+	if appengine.IsDevAppServer() {
+		return "http://"
+	}
+	return "https://"
 }
